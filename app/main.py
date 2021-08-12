04cb45e79457c47316538a478f87e850303dba28
melkdesousa <melk.sousap@gmail.com>
2021-08-12 20:06:42 -0300

    fix: access methods in 'cli' group, but correct is 'click' class

--- app/main.py
+++ app/main.py
@@ -8,12 +8,13 @@ import math
 database = db.getDb('todo.db.json')
 
 
-def template_time(days=0, hours=0, minutes=0):
-    template_days = f'{days} days' if days > 0 else ""
+def template_time(month=0, days=0, hours=0, minutes=0):
+    template_month = f'{month} month(s)' if month > 0 else ""
+    template_days = f'{days} days' if 0 < days < 30 else ""
     template_hours = f'{hours} hours' if hours > 0 else ""
     template_minutes = f'{" and " if template_hours  else ""}{minutes} minutes' if minutes > 0 else ""
 
-    return f'{f"{template_days}, " if template_days else ""}{f"{template_hours}" if template_hours else ""}{template_minutes}'
+    return f'{f"{template_month}, " if template_month else ""}{f"{template_days}, " if template_days else ""}{f"{template_hours}" if template_hours else ""}{template_minutes}'
 
 
 def get_age(total_seconds: int):
@@ -21,11 +22,15 @@ def get_age(total_seconds: int):
     SECONDS_IN_HOUR = 60 * 60
     SECONDS_IN_DAY = SECONDS_IN_HOUR * 24
 
-    days = math.floor(total_seconds // SECONDS_IN_DAY)
-    hours = math.floor((total_seconds - (days * SECONDS_IN_DAY)) // SECONDS_IN_HOUR)
-    minutes = math.floor((total_seconds - (days * SECONDS_IN_DAY) - (hours * SECONDS_IN_HOUR)) // SECONDS_IN_MINUTES)
+    month = math.floor(total_seconds // SECONDS_IN_DAY * 30)
 
-    return template_time(days, hours, minutes)
+    days = math.floor(total_seconds // SECONDS_IN_DAY)
+    hours = math.floor(
+        (total_seconds - (days * SECONDS_IN_DAY)) // SECONDS_IN_HOUR)
+    minutes = math.floor((total_seconds - (days * SECONDS_IN_DAY) -
+                         (hours * SECONDS_IN_HOUR)) // SECONDS_IN_MINUTES)
+
+    return template_time(month, days, hours, minutes)
 
 
 @click.group()
@@ -34,8 +39,8 @@ def cli():
 
 
 @cli.command('add')
-@cli.argument('description')
-@cli.option('--priority', '-p', default='NORMAL', help='Priority task [LOW, NORMAL, HIGH]')
+@click.argument('description')
+@click.option('--priority', '-p', default='NORMAL', help='Priority task [LOW, NORMAL, HIGH]')
 def add(description: str, priority: str):
     task = {
         'description': description,
@@ -51,7 +56,7 @@ def add(description: str, priority: str):
 
 
 @cli.command('complete', help='Mark task by ID with DONE')
-@cli.argument('id')
+@click.argument('id')
 def complete(id: int):
     task = database.find(id)
     task_done = {
@@ -66,7 +71,7 @@ def complete(id: int):
 
 
 @cli.command('delete')
-@cli.argument('id')
+@click.argument('id')
 def delete(id: int):
     database.deleteById(id)
 
@@ -80,7 +85,9 @@ def list():
     new_tasks = [
         {
             **task,
-            'age': get_age((datetime.now() - datetime.fromisoformat(task['created_at'])).total_seconds())
+            'age': get_age((
+                datetime.now() - datetime.fromisoformat(task['created_at'])).total_seconds()
+            )
         }
         for task in tasks
     ]
